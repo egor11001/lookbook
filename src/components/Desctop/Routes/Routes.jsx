e0d2efb08c3f9ce780ee2b6f48d7db50596b8507
Egor <egorlogvinov18@yandex.ru>
2022-06-25 17:44:03 +0800

    responsive

--- src/components/Desctop/Routes/Routes.jsx
+++ src/components/Desctop/Routes/Routes.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Routes as Switch, Route } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
+
 import BasketPage from '../../../pages/BasketPage';
 import MainPage from '../../../pages/MainPage';
 import ItemPage from '../../../pages/ItemPage';
@@ -13,24 +15,37 @@ import CollaborationInfoPage from '../../../pages/infos/CollaborationInfoPage';
 import InvestorsInfoPage from '../../../pages/infos/InvestorsInfoPage';
 import PrivacyPolicyPage from '../../../pages/infos/PrivacyPolicyPage';
 import Ordering from '../../../pages/Ordering';
+import Hello from '../../Mobile/Hello';
 
 const Routes = () => {
+  const isDesktop = useMediaQuery({
+    query: '(min-width: 1224px)',
+  });
+  const isMobile = useMediaQuery({
+    query: '(max-width: 480px)',
+  });
   return (
     <Switch>
-      <Route path="/" element={<Layout />}>
-        <Route path="" element={<MainPage />} />
-        <Route path="item" element={<ItemPage />} />
-        <Route path="basket" element={<BasketPage />} />
-        <Route path="ordering" element={<Ordering />} />
-        <Route path="about" element={<AboutUsPage />} />
-        <Route path="contacts" element={<ContactsPage />} />
-        <Route path="payment" element={<PaymentInfoPage />} />
-        <Route path="delivery" element={<DeliveryInfoPage />} />
-        <Route path="collaboration" element={<CollaborationInfoPage />} />
-        <Route path="investors" element={<InvestorsInfoPage />} />
-        <Route path="privacy-policy" element={<PrivacyPolicyPage />} />
-        <Route path="FAQ" element={<FAQPage />} />
-      </Route>
+      {isDesktop ? (
+        <Route path="/" element={<Layout />}>
+          <Route path="" element={<MainPage />} />
+          <Route path="item" element={<ItemPage />} />
+          <Route path="basket" element={<BasketPage />} />
+          <Route path="ordering" element={<Ordering />} />
+          <Route path="about" element={<AboutUsPage />} />
+          <Route path="contacts" element={<ContactsPage />} />
+          <Route path="payment" element={<PaymentInfoPage />} />
+          <Route path="delivery" element={<DeliveryInfoPage />} />
+          <Route path="collaboration" element={<CollaborationInfoPage />} />
+          <Route path="investors" element={<InvestorsInfoPage />} />
+          <Route path="privacy-policy" element={<PrivacyPolicyPage />} />
+          <Route path="FAQ" element={<FAQPage />} />
+        </Route>
+      ) : isMobile ? (
+        <Route path="/" element={<Hello />}></Route>
+      ) : (
+        console.log(' wow new technology !')
+      )}
     </Switch>
   );
 };
