2697b652575eeeff227f16c2dd53f7e654f7cc1f
Egor <egorlogvinov18@yandex.ru>
2022-07-04 16:42:07 +0800

    fix5

--- src/components/Mobile/ScrollButton.jsx
+++ src/components/Mobile/ScrollButton.jsx
@@ -16,7 +16,7 @@ const ScrollButton = () => {
   };
 
   const scrollToTop = () => {
-    window.scrollTo({
+    document.body.scrollTo({
       top: 0,
       behavior: 'smooth',
       /* you can also use 'auto' behaviour
